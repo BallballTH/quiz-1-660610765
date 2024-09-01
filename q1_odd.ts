39bff5da42645ed1402541839b375759e9f64e55
BallChon <Buranin_b@cmu.ac.th>
2024-09-01 19:20:04 +0700

    Done Odd

--- q1_odd.ts
+++ q1_odd.ts
@@ -1,5 +1,14 @@
 /* assign interface/type to the function definition properly */
-
+const q1_odd = (m:number,n:number):number =>{
+    let sum:number = 0;
+    for(let i:number = m+1;i<n;i++){
+      if(i%2 == 1){
+        sum += i ;
+      }
+    }
+    return sum;
+  }
+  
 
 //test cases
 console.log(q1_odd(1, 3));
